4ac8747f7b87634855a16bae139f722eacecb23f
Mikael Lind <elemel@elemel.se>
2014-12-04 22:53:24 +0100

    Move fbm functions to math modules

--- math2D.lua
+++ math2D.lua
@@ -1,8 +1,11 @@
-local function fbm(x, noise, octave, lacunarity, gain)
+local math2D = {}
+
+function math2D.fbm(x, y, noise, octave, lacunarityX, lacunarityY, gain)
     noise = noise or love.math.noise
     octave = octave or 3
-    lacunarity = lacunarity or 2
-    gain = gain or 1 / lacunarity
+    lacunarityX = lacunarityX or 2
+    lacunarityY = lacunarityY or 2
+    gain = gain or 2 / (lacunarityX + lacunarityY)
 
     local integralOctave, fractionalOctave = math.modf(octave)
     local amplitude = 1
@@ -11,19 +14,20 @@ local function fbm(x, noise, octave, lacunarity, gain)
     local totalAmplitude = 0
 
     for i = 1, integralOctave do
-        totalNoise = totalNoise + amplitude * noise(x, 0)
+        totalNoise = totalNoise + amplitude * noise(x, y)
         totalAmplitude = totalAmplitude + amplitude
 
-        x = x * lacunarity
+        x = x * lacunarityX
+        y = y * lacunarityY
         amplitude = amplitude * gain
     end
 
     if fractionalOctave > 0 then
-        totalNoise = totalNoise + fractionalOctave * amplitude * noise(x)
+        totalNoise = totalNoise + fractionalOctave * amplitude * noise(x, y)
         totalAmplitude = totalAmplitude + fractionalOctave * amplitude
     end
 
     return totalNoise / totalAmplitude
 end
 
-return {fbm = fbm}
+return math2D

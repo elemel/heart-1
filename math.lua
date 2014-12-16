4a77ba5f476021c26379d334594fa6e090aadff9
Mikael Lind <elemel@elemel.se>
2014-12-16 15:29:44 +0100

    Merge colors module into math

--- math.lua
+++ math.lua
@@ -64,6 +64,34 @@ function math_.normalize2(x, y)
     return x / length, y / length, length
 end
 
+-- Adapted from Taehl: http://love2d.org/wiki/HSL_color
+function math_.toRgbFromHsl(h, s, l)
+    if s <= 0 then
+        return l, l, l
+    end
+
+    h = h * 6
+    local c = (1 - math.abs(2 * l - 1)) * s
+    local x = (1 - math.abs(h % 2 - 1)) * c
+    local m, r, g, b = (l - 0.5 * c), 0, 0, 0
+
+    if h < 1 then
+        r, g, b = c, x, 0
+    elseif h < 2 then
+        r, g, b = x, c, 0
+    elseif h < 3 then
+        r, g, b = 0, c, x
+    elseif h < 4 then
+        r, g, b = 0, x, c
+    elseif h < 5 then
+        r, g, b = x, 0, c
+    else
+        r, g, b = c, 0, x
+    end
+
+    return r + m, g + m, b + m
+end
+
 function math_.fbm(x, noise, octave, lacunarity, gain)
     noise = noise or love.math.noise
     octave = octave or 3
